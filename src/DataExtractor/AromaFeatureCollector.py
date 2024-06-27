2819e60050456cee1a76ccb6a8d3f8d46119d8c7
long <ngp143@usask.ca>
2024-06-27 11:39:23 -0600

    Finished Visit_If. Added toString to print the tree

--- src/DataExtractor/AromaFeatureCollector.py
+++ src/DataExtractor/AromaFeatureCollector.py
@@ -1,27 +1,32 @@
 from ast import *
 import ast
-from anytree import Node, NodeMixin, RenderTree
+from anytree import Node, NodeMixin, RenderTree, node
 from queue import Queue
 
-def extract_aroma_tree(file):
-    queue = Queue()
-    root = Node()
 
+def extract_aroma_tree(file):
     class Position():
-        def __init(self, lineno, col_offset, end_lineno, end_col_offset):
+        def __init__(self, lineno, col_offset, end_lineno, end_col_offset):
             self.lineno = lineno
             self.col_offset = col_offset
             self.end_lineno = end_lineno
             self.end_col_offset = end_col_offset
 
+        def __repr__(self):
+            return " line: " + str(self.lineno) + " - endline: " + str(self.end_lineno) + " | col_offset: " + str(self.col_offset) + " - end_col_offset: " + str(self.end_col_offset)
+            
+
     class MyAnyTreeNode(NodeMixin):  # Add Node feature
-         def __init__(self, label, position, parent=None, children=None):
+        def __init__(self, label, position, parent=None, children=None):
             super().__init__()
             self.label = label
             self.position = position
             self.parent = parent
             if children:
                 self.children = children
+
+        def __repr__(self):
+            return node.util._repr(self)
 
     class MyVisitor (ast.NodeVisitor):
         def visit(self, node, parent_AnyTree_Node):
@@ -36,28 +41,54 @@ def extract_aroma_tree(file):
                 if isinstance(value, list):
                     for item in value:
                         if isinstance(item, AST):
-                            self.visit(item, parent_AnyTree_Node)
+                            return self.visit(item, parent_AnyTree_Node)
                 elif isinstance(value, AST):
-                    self.visit(value, parent_AnyTree_Node)
+                    return self.visit(value, parent_AnyTree_Node)
 
         #Control Flow
         def visit_If(self, node, parent):
             position = Position(node.lineno, node.col_offset, node.end_lineno, node.end_col_offset)                
             
-            if parent:
-                if_node = MyAnyTreeNode("if##", position, parent)
+            if_AnyTreeNode = MyAnyTreeNode("if##", position, parent)
+
+            if_conditional_AnyTreeNode = MyAnyTreeNode("(#)", position, if_AnyTreeNode)
+
+            #Traverse through the conditional statement
+            conditional_node = node.test
+            self.generic_visit(conditional_node, if_conditional_AnyTreeNode)
+
+            if_body_label = ":"
+            for i in range ( len(node.body)):
+                if_body_label = if_body_label + "#"
+
+            if_body_AnyTreeNode = MyAnyTreeNode(if_body_label, position, if_AnyTreeNode)
             
-            #Traverse through the conditional statement and Body
-            self.generic_visit(node, if_node)
+            #Traverse through statements in the body
+            for childNode in node.body:
+                self.generic_visit(childNode, if_body_AnyTreeNode)
 
             #Traverse through else statement
+            else_AnyTreeNode = MyAnyTreeNode("else#", position, parent)
+
+            else_body_label = ":"
+
+            #TODO: since else node is inside if node, we would only have 1 # instead of 2 #s.
+            for i in range ( len(node.orelse)):
+                else_body_label = else_body_label + "#"
+
+            #Get lineno of else token
+            else_lineno = node.body[-1].end_lineno + 1
+
+            #TODO: end col offset might not be correct. Fix this later
+            position = Position(else_lineno, node.col_offset, node.end_lineno, node.end_col_offset)
+            else_body_AnyTreeNode = MyAnyTreeNode(else_body_label, position, else_AnyTreeNode)
             
+            for childNode in node.orelse:
+                self.generic_visit(childNode, else_body_AnyTreeNode)
 
-
-
-
-
+            return if_AnyTreeNode
 
     tree = ast.parse(file.read())
     visitor = MyVisitor()
-    visitor.visit(tree, None)
+    print(RenderTree(visitor.visit(tree, None)))
+    
