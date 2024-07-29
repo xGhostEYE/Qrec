0511a510224b862e54eb33b43bfd658bca2eaa04
Melvin Lugo <melvin.lugo@hotmail.com>
2024-07-29 17:43:13 -0600

    passpdf.py testfile made and testing all aroma

--- train/Project1/Subfolder/passpdf.py
+++ train/Project1/Subfolder/passpdf.py
@@ -1,71 +1,151 @@
-from PyPDF2 import PdfFileWriter as writer, PdfFileReader
-import getpass
+# Root nodes
+module = "import math"
 
-# Making an instance of the PdfFileWriter class and storing it in a variable
-writer = PdfFileWriter()
-writer.addPage("wow")
+# Literals
+num = 42
+string = "Alice"
+bytes_literal = b'hello'
+ellipsis = ...
+name_constant = True
 
-global x,y,z
- 
+# Variables
+name = "x"
 
-def test():
+# Expressions
+bin_op = 2 + 3
+unary_op = -5
+lambda_expr = lambda x: x * 2
+if_exp = x if x > 0 else -x
+dict_literal = {'name': 'Alice', 'age': 30}
+set_literal = {1, 2, 3}
+list_comp = [x * x for x in range(5)]
+set_comp = {x for x in range(10) if x % 2 == 0}
+dict_comp = {word: len(word) for word in ['apple', 'banana']}
+generator_exp = (x * x for x in range(5))
+await_expr = await async_function()
+yield_expr = yield 42
+yield_from_expr = yield from another_generator()
+compare_op = x < 5
+call = print('Hello, world!')
+subscript = my_list[2]
+attribute = person.name
+starred = *args
+named_expr = (n := len(my_list)) > 0
+
+# Subscripting
+index = my_list[0]
+slice = my_list[1:3]
+ext_slice = my_list[1:3, ...]
+
+# Comprehensions
+comprehension = [x for x in range(5)]
+
+# Statements
+# assign
+x = 10
+# aug_assign
+count += 1
+# ann_assign
+x: int = 42
+# for_loop
+for i in range(5):
     pass
-def test2(something):
+# while_loop
+while condition:
     pass
-
-writer = "something"
-
-writer.addPage("wow")
-# Explicitly ask the user what the name of the original file is
-pdf_name = input('Pleast type in the name of the pdf file suffixed with its extention: ')
-
-# Making an instance of the PdfFileReader class with the original file as an argument
-original_file = PdfFileReader(*pdf_name)
-
-# Copies the content of the original file to the writer variable
-for page in range(original_file.numPages):
-    writer.addPage(original_file.getPage(page))
-
-def something(a, b):
+# if_stmt
+if x > 0:
     pass
-
-
-assert x,y
-
-
-def f(a: 'annotation', b=1, c=2, *d, e=3, f, **g) -> 'return annotation':
-    return 9
-
-if a or b and c and not d:
-    pass
+# else_stmt
 else:
-    if something:
-        pass
-    elif something:
-        pass
-    else:
-        pass
-
-while something:
+    print("hat")
+# with_stmt
+with open('file.txt', 'r') as f:
     pass
-else:
+# raise_stmt
+raise ValueError('Error message')
+# try_except
+try:
     pass
-# Retrieve a preferred password from the user 
-password = getpass.getpass(prompt = "Set a Password: ")
+except Exception:
+    pass
+# assert_stmt
+assert condition, 'Assertion failed'
+# import_from
+from math import pi
+# global_stmt
+global x
+# nonlocal_stmt
+nonlocal y
+# expr_stmt
+print('Hello')
+# pass_stmt
+pass
+# delete_stmt
+del x
+# return_stmt
+return value
+# break_stmt
+break
+# continue_stmt
+continue
 
-# Encrypt the copy of the original file
-writer.encrypt(password)
+# Control Flow
+# if_stmt
+if condition:
+    pass
+# for_loop
+for i in range(5):
+    pass
+# while_loop
+while condition:
+    pass
+# try_except
+try:
+    pass
+except Exception:
+    pass
+# with_stmt
+with open('file.txt', 'r') as f:
+    pass
 
-# Opens a new pdf (write brinary permission) and writes the content of the 'writer' into it
-with open('secured.pdf', 'wb') as f:
-    f.close()
+# Pattern Matching
+    match x:
+        case 1:
+            pass
 
-lambda x,y: ...
+# Type Parameters
 
-a if b else c
+# type_param
+def generic_function[T](x: T) -> T:
+    pass
+# param_spec
+def generic_function[P](x: P) -> P:
+    pass
+# constr
+def generic_function[T: int](x: T) -> T:
+    pass
 
-{"a":1, **d}
+# Function and Class Definitions
+# function_def
+def my_function(x):
+    pass
+# async_function_def
+async def async_function():
+    pass
+# class_def
+class MyClass:
+    pass
+# async_for
+async for item in async_iterator:
+    pass
+# async_with
+async with async_context_manager():
+    pass
 
-l[1:2, 3]
-l[1:2]
-l[4]
+# Async and Await
+# async_stmt
+async def async_function():
+    pass
+# await_expr
+await async_task()
