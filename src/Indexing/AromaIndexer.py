d56a2230ed4b6786252eb51349a63a7857f2c5f9
Melvin Lugo <melvin.lugo@hotmail.com>
2024-08-06 22:35:17 -0600

    new files for next aroma stage

---
+++ src/Indexing/AromaIndexer.py
@@ -0,0 +1,12 @@
+from whoosh.index import create_in
+from whoosh.fields import *
+
+# index section
+
+
+
+# searching section
+
+
+
+# evalution section (determine which feature has the most impact on performance)
