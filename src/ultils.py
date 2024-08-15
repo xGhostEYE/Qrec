102a1bb776890b46c8b581e8582b8e5ab357039a
long <ngp143@usask.ca>
2024-08-15 13:42:49 -0600

    pushed changes for the progress bar. Pushed experimental LM model. Made some changes in DataEncoder

--- src/ultils.py
+++ src/ultils.py
@@ -37,71 +37,87 @@ def create_pyart_dataset(directory, csv_path):
         directoryPath.append(file_path)
     
     #For each projects in directory
-    for path in tqdm(directoryPath):
-        # print("\nExecuting Project: " + path + " | Progress: " + str(directoryPath.index(path) + 1)+ "/" + str(len(directoryPath)))
-        file_dict = {}
+    with tqdm(directoryPath, total = len(directoryPath)) as t:
+        for path in t:
+            t.set_description("Executing Project: %s. Current Progress:" %path)
+            print("[LOGGING] Executing Project: " + path + " | project number/total projects : " + str(directoryPath.index(path) + 1)+ "/" + str(len(directoryPath)))
+            elapsed = t.format_dict['elapsed']
+            elapsed_str = t.format_interval(elapsed)            
+            rate = t.format_dict["rate"]
+            remaining = (t.total - t.n) / rate if rate and t.total else 0
+            print("Elapsed: " + elapsed_str, "| Rate: " + str(rate), "| Remaining (seconds): " + str(remaining) + "\n")
 
+            file_dict = {}
 
-        #Stores frequency of tokens in EACH file    
-        frequency_file_dict = {}
-        #Stores frequency of tokens in ALL files    
-        frequency_files_dict = {}
+            #Stores frequency of tokens in EACH file    
+            frequency_file_dict = {}
+            #Stores frequency of tokens in ALL files    
+            frequency_files_dict = {}
 
-        #Stores occurence of tokens in EACH file    
-        occurrence_file_dict = {}
-        #Stores occurence of tokens in ALL files    
-        occurrence_files_dict = {}
+            #Stores occurence of tokens in EACH file    
+            occurrence_file_dict = {}
+            #Stores occurence of tokens in ALL files    
+            occurrence_files_dict = {}
 
-        try:
-            # os.walk NEEDS a subdirectory in the directory that it's walking for it to work.
-            # meaning that it needs 2 layers of folders to work
-            for root, directories, files in os.walk(path, topdown=False):
-                for name in files:
-                    file_path = (os.path.join(root, name))
-                    
-                    if file_path.endswith(".py") or file_path.endswith(".pyi"):
-                        try: 
-                            #Key: file_name
-                            #Value: bag of tokens (a dictionary)
-                                    #Key: line number
-                                    #Value: list of tokens from left to right 
-                            with open(file_path, encoding='utf-8') as file:
-                                frequency_dict = {}
-                                occurrence_dict = {}
-                                
-                                file_dict[file_path] = fc.extract_bag_of_tokens(file, frequency_dict, occurrence_dict)
-                                frequency_file_dict[file_path] = frequency_dict
-                                occurrence_file_dict[file_path] = occurrence_dict
-                            
+            try:
+                # os.walk NEEDS a subdirectory in the directory that it's walking for it to work.
+                # meaning that it needs 2 layers of folders to work
+                for root, directories, files in os.walk(path, topdown=False):
+                    for name in files:
+                        file_path = (os.path.join(root, name))
                         
-                        except Exception as e:
-                            print(f"Error processing file dictionary for '{file_path}': {e}")
-                            traceback.print_exc()   
+                        if file_path.endswith(".py") or file_path.endswith(".pyi"):
+                            try: 
+                                #Key: file_name
+                                #Value: bag of tokens (a dictionary)
+                                        #Key: line number
+                                        #Value: list of tokens from left to right 
+                                with open(file_path, encoding='utf-8') as file:
+                                    frequency_dict = {}
+                                    occurrence_dict = {}
+                                    
+                                    file_dict[file_path] = fc.extract_bag_of_tokens(file, frequency_dict, occurrence_dict)
+                                    frequency_file_dict[file_path] = frequency_dict
+                                    occurrence_file_dict[file_path] = occurrence_dict
+                                
+                            
+                            except Exception as e:
+                                print(f"Error processing file dictionary for '{file_path}': {e}")
+                                traceback.print_exc()   
 
-            list_all_file_path = list(file_dict.keys())
+                list_all_file_path = list(file_dict.keys())
 
-            for file_path in list_all_file_path:     
-                try:
-                    with open(file_path, encoding='utf-8') as file:
-                        method_dict = fc.extract_data(file)
+                for file_path in list_all_file_path:     
+                    try:
+                        with open(file_path, encoding='utf-8') as file:
+                            method_dict = fc.extract_data(file)
 
-                    with open(file_path, encoding='utf-8') as file:
-                        default_calls = stdlibs_calls.copy()
-                        default_calls.update(cg.get_calls_from_third_party_libs(file_path))
-                        default_calls.update(cg.get_calls_from_scope(file_path))
-                        candidate_dict = cg.CandidatesGenerator(file, file_path, method_dict, default_calls)
-                    
-                    #Format of data_dict:
-                    # #Key = [object, api, line number, 0 if it is not true api and 1 otherwise]
-                    # #Value = [x1,x2,x3,x4]
-                    data_dict = de.DataEncoder(method_dict,candidate_dict, file_dict, list_all_file_path, file_path, frequency_files_dict, frequency_file_dict, occurrence_files_dict, occurrence_file_dict)
-                    write_pyart_csv_data(data_dict, csv_path, file_path)
-                except Exception as e:
-                    print(f"Error processing during data encoding stage for '{file_path}': {e}")
-                    traceback.print_exc()
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
+                        with open(file_path, encoding='utf-8') as file:
+                            default_calls = stdlibs_calls.copy()
+                            default_calls.update(cg.get_calls_from_third_party_libs(file_path))
+                            default_calls.update(cg.get_calls_from_scope(file_path))
+                            candidate_dict = cg.CandidatesGenerator(file, file_path, method_dict, default_calls)
+                        
+                        #Format of data_dict:
+                        # #Key = [object, api, line number, 0 if it is not true api and 1 otherwise]
+                        # #Value = [x1,x2,x3,x4]
+                        data_dict = de.DataEncoder(method_dict,candidate_dict, file_dict, list_all_file_path, file_path, frequency_files_dict, frequency_file_dict, occurrence_files_dict, occurrence_file_dict)
+                        write_pyart_csv_data(data_dict, csv_path, file_path)
+                    except Exception as e:
+                        print(f"Error processing during data encoding stage for '{file_path}': {e}")
+                        traceback.print_exc()
+            except Exception as e:
+                print(e)
+                traceback.print_exc()
+
+            print("\n")
+            t.update(1)
+            t.refresh()
+    
+        elapsed = t.format_dict['elapsed']
+        elapsed_str = t.format_interval(elapsed)  
+        print("Finished creating csv file with runtime: " + elapsed_str)
+
 
 def write_pyart_csv_data(data_dict, csv_file_path, file_path):
     fields = ["file_path", "object", "api", "line_number", "is_true_api", "x1", "x2", "x3", "x4"]
