f2cb18bb049cd165f20190d23f241c10706752b8
long <ngp143@usask.ca>
2024-10-17 18:20:58 -0600

    Add progress bar for aroma testing

--- src/Indexing/AromaIndexer.py
+++ src/Indexing/AromaIndexer.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from whoosh.index import create_in, open_dir
 from whoosh.fields import *
 from whoosh.query import *
@@ -60,56 +61,63 @@ def search_data(test_csv_file_path, top_k = None):
             #Key: (true method)
             #Value: list of recommendations (sorted)
             recommendation_dict = {}
-            for row in data_reader:
-                
-                file_path = row[column_indices[0]]
-                receiver = row[column_indices[2]]
-                method = row[column_indices[3]]
+            with tqdm(data_reader, total = len(data_reader)) as t:
 
-                token_feature = row[column_indices[4]]
-                parent_feature = row[column_indices[5]]
-                sibling_feature = row[column_indices[6]]
-                variable_usage_feature = row[column_indices[7]]
+                for row in t:
+                    elapsed = t.format_dict['elapsed']
+                    elapsed_str = t.format_interval(elapsed)            
+                    rate = t.format_dict["rate"]
+                    remaining = (t.total - t.n) / rate if rate and t.total else 0
+                    print("Elapsed: " + elapsed_str, "| Rate: " + str(rate), "| Remaining (seconds): " + str(remaining) + "\n")
 
-                
-                # feature_query = Or([Term("token_feature", token_feature), Term("parent_feature", parent_feature), Term("sibling_feature", sibling_feature), Term("variable_usage_feature", variable_usage_feature)])
-                # recomemnded_methods_call = searcher.query(feature_query, limit=top_k)
-                
-                token_feature_query = QueryParser("token_feature", token_feature).parse(token_feature)
-                parent_feature_query = QueryParser("parent_feature", token_feature).parse(parent_feature)
-                sibling_feature_query = QueryParser("sibling_feature", token_feature).parse(sibling_feature)
-                variable_usage_feature_query = QueryParser("variable_usage_feature", token_feature).parse(variable_usage_feature)
+                    file_path = row[column_indices[0]]
+                    receiver = row[column_indices[2]]
+                    method = row[column_indices[3]]
 
-                list_feature_queries = [token_feature_query,parent_feature_query,sibling_feature_query,variable_usage_feature_query]
-                
-                search_result_dict = {}
-                for index in range(len(list_feature_queries)):
-                    feature_query = list_feature_queries[index]
+                    token_feature = row[column_indices[4]]
+                    parent_feature = row[column_indices[5]]
+                    sibling_feature = row[column_indices[6]]
+                    variable_usage_feature = row[column_indices[7]]
 
-                    #Top K results for each feature-search
-                    results = searcher.search(feature_query, limit = top_k)
+                    
+                    # feature_query = Or([Term("token_feature", token_feature), Term("parent_feature", parent_feature), Term("sibling_feature", sibling_feature), Term("variable_usage_feature", variable_usage_feature)])
+                    # recomemnded_methods_call = searcher.query(feature_query, limit=top_k)
+                    
+                    token_feature_query = QueryParser("token_feature", token_feature).parse(token_feature)
+                    parent_feature_query = QueryParser("parent_feature", token_feature).parse(parent_feature)
+                    sibling_feature_query = QueryParser("sibling_feature", token_feature).parse(sibling_feature)
+                    variable_usage_feature_query = QueryParser("variable_usage_feature", token_feature).parse(variable_usage_feature)
 
-                    #Saving the occurance of a method_call for a feature-search
-                    for matched_document in results:
-                        method_call = matched_document['method']
-                        if method_call in search_result_dict:
-                            method_result_list = search_result_dict[method_call]
-                            method_result_list[index] = method_result_list[index] + 1
-                        else:
-                            method_result_list = [0,0,0,0]
-                            method_result_list[index] = 1
-                            search_result_dict[method_call] = method_result_list
+                    list_feature_queries = [token_feature_query,parent_feature_query,sibling_feature_query,variable_usage_feature_query]
+                    
+                    search_result_dict = {}
+                    for index in range(len(list_feature_queries)):
+                        feature_query = list_feature_queries[index]
 
-                w1 = 0.25
-                w2 = 0.25
-                w3 = 0.25
-                w4 = 0.25
+                        #Top K results for each feature-search
+                        results = searcher.search(feature_query, limit = top_k)
 
-                def sort(item):
-                    return w1*item[1][0] + w2*item[1][1] + w3*item[1][2] + w4*item[1][3]                
-                sorted_search_result_dict = dict(sorted(search_result_dict.items(), key=sort, reverse=True))
-                recommendation_dict[method] = list(sorted_search_result_dict.keys())
-            evaluate_result(recommendation_dict)
+                        #Saving the occurance of a method_call for a feature-search
+                        for matched_document in results:
+                            method_call = matched_document['method']
+                            if method_call in search_result_dict:
+                                method_result_list = search_result_dict[method_call]
+                                method_result_list[index] = method_result_list[index] + 1
+                            else:
+                                method_result_list = [0,0,0,0]
+                                method_result_list[index] = 1
+                                search_result_dict[method_call] = method_result_list
+
+                    w1 = 0.25
+                    w2 = 0.25
+                    w3 = 0.25
+                    w4 = 0.25
+
+                    def sort(item):
+                        return w1*item[1][0] + w2*item[1][1] + w3*item[1][2] + w4*item[1][3]                
+                    sorted_search_result_dict = dict(sorted(search_result_dict.items(), key=sort, reverse=True))
+                    recommendation_dict[method] = list(sorted_search_result_dict.keys())
+                evaluate_result(recommendation_dict)
                 
     # search each of the features using top k as the limit, and use the pyart similarity score in the paper which gives a number
     # repeat for all the other features and then i get 4 scores in total
